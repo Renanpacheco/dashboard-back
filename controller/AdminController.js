d0306e228ce377adde67b811a45e9f79f827f985
Renanpacheco <prenan86@gmail.com>
2025-09-26 19:02:20 -0300

    add new admin

--- controller/AdminController.js
+++ controller/AdminController.js
@@ -1,7 +1,28 @@
 const Admin = require("../models/Admin");
+const bcrypt = require("bcrypt");
 
-module.exports = class UserController {
+module.exports = class AdminController {
   static async register(req, res) {
-    res.json("test");
+    const { name, email, password } = req.body;
+
+    const adminExists = await Admin.findOne({ email: email });
+
+    if (adminExists) {
+      res.status(422).json("this account already exists");
+    }
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const admin = new Admin({
+      name: name,
+      email: email,
+      password: passwordHash,
+    });
+    try {
+      const newAdmin = await admin.save();
+      res.status(201).json(newAdmin);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   }
 };
